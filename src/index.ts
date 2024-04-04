529968eba7152c9efc640995a0a218738d338dd2
tomoyanakano <tomo1999724@gmail.com>
2024-04-04 15:00:36 +0900

    feat(admin-api-client): Add AdminGraphQLApiClient function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { AdminGraphQLApiClient, createAdminGraphQLApiClient_ } from "./admin-api-client/graphql/client";
 import { AdminRestApiClient, createAdminRestApiClient_ } from "./admin-api-client/rest/client";
 
 /**
@@ -8,12 +9,32 @@ import { AdminRestApiClient, createAdminRestApiClient_ } from "./admin-api-clien
  * @param {string} [apiVersion=Latest Api Version] - The version of the API to use
  * @returns {AdminRestApiClient} - The instance of the AdminRestApiClient
  * */
-function restClient(
+function adminRestClient(
   storeDomain: string,
   accessToken: string,
   apiVersion: string,
 ): AdminRestApiClient {
   return createAdminRestApiClient_({
+    storeDomain,
+    accessToken,
+    apiVersion,
+  })
+}
+
+/**
+ * Create a new instance of the AdminGraphQLApiClient
+ *
+ * @param {string} storeDomain - The domain of the store
+ * @param {string} accessToken - The access token to authenticate the requests
+ * @param {string} [apiVersion=Latest Api Version] - The version of the API to use
+ * @returns {AdminGraphQLApiClient} - The instance of the AdminGraphQLApiClient
+ * */
+function adminGraphQLClient(
+  storeDomain: string,
+  accessToken: string,
+  apiVersion: string,
+): AdminGraphQLApiClient {
+  return createAdminGraphQLApiClient_({
     storeDomain,
     accessToken,
     apiVersion,
